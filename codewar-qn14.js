58793fb5d40570fc5d9a9e4996381bc56d4aba15
Neeharika Ramu <58494945+neeharikaramu@users.noreply.github.com>
2022-04-14 22:48:28 +0400

    My first 7kyu! List Filtering

---
+++ codewar-qn14.js
@@ -0,0 +1,15 @@
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+    function filter_list(l) {
+        return l.filter(value => typeof value === 'number') 
+       }
+
+       console.log(filter_list(['a','b',0,1,2])); //const filter_list = array => array.filter(a => typeof(a)==='number').filter(a => a >= 0) for non negative numbers
+
+
+    //    List Filtering. My first 7kyu!
